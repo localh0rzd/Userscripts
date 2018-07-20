463521aebee654d6f19b57cb6f6948f6ea256692
Andrej Beliakov <beliakov@optimal-systems.de>
2018-07-20 16:45:00 +0200

    add jira script

---
+++ jira_context_menu.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Improve JIRA context menu
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Because context menus should not be skyscrapers
+// @author       localh0rzd
+// @updateURL    https://github.com/localh0rzd/Userscripts/raw/master/jira_context_menu.user.js
+// @match        http*://*/secure/RapidBoard.jspa*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+document.addEventListener("contextmenu", e => {
+    try{
+        const menu = document.querySelector("#gh-ctx-menu-trigger_drop");
+        menu.style.overflow = "scroll"
+        if(e.clientY > (window.innerHeight / 2)){
+            menu.style.top = `${e.clientY - 400}px`
+        } else {
+            menu.style.top = `${e.clientY}px`
+        }
+        menu.style.height = "400px"
+    } catch(error) {
+    }
+    
+})
+})();
