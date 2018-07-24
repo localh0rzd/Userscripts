5a64b4697e83cc8936c2cfbd3cc1598e98a5cda6
Andrej Beliakov <beliakov@optimal-systems.de>
2018-07-24 14:57:45 +0200

    remove useless x-scrollbar

--- jira_context_menu.user.js
+++ jira_context_menu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Improve JIRA context menu
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Because context menus should not be skyscrapers
 // @author       localh0rzd
 // @updateURL    https://github.com/localh0rzd/Userscripts/raw/master/jira_context_menu.user.js
@@ -13,15 +13,13 @@
 document.addEventListener("contextmenu", e => {
     try{
         const menu = document.querySelector("#gh-ctx-menu-trigger_drop");
-        menu.style.overflow = "scroll"
+        menu.style.overflowY = "scroll"
         if(e.clientY > (window.innerHeight / 2)){
             menu.style.top = `${e.clientY - 400}px`
         } else {
             menu.style.top = `${e.clientY}px`
         }
         menu.style.height = "400px"
-    } catch(error) {
-    }
-    
+    } catch(error) {}
 })
 })();
