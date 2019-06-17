218645dd1604c28240c35aeebf324fbda96bf43a
Andrej Beliakov <beliakov@optimal-systems.de>
2019-06-17 11:17:54 +0200

    remove IIFE

--- jira_context_menu.user.js
+++ jira_context_menu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Improve JIRA context menu
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Because context menus should not be skyscrapers
 // @author       localh0rzd
 // @updateURL    https://github.com/localh0rzd/Userscripts/raw/master/jira_context_menu.user.js
@@ -11,17 +11,18 @@
 
 GM_addStyle(".ghx-avatar-img { width: 40px !important; height: 40px !important; }")
 
-(function() {
 document.addEventListener("contextmenu", e => {
     try{
         const menu = document.querySelector("#gh-ctx-menu-trigger_drop");
         menu.style.overflowY = "scroll"
+        console.info(e)
+        console.info(window.innerHeight)
         if(e.clientY > (window.innerHeight / 2)){
             menu.style.top = `${e.clientY - 400}px`
         } else {
             menu.style.top = `${e.clientY}px`
         }
         menu.style.height = "400px"
-    } catch(error) {}
+    } catch(error) {
+    console.warn(error)}
 })
-})();
